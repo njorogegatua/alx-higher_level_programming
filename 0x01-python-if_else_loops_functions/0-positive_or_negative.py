e5a4fe45f7535a415c34c4917923dbdb181b13db
njorogegatua <102298471+njorogegatua@users.noreply.github.com>
2022-07-14 12:54:18 +0300

    Update 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -7,9 +7,3 @@ elif number == 0:
     print("{:d} is zero".format(number))
 else:
     print("{:d} is negative".format(number))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
