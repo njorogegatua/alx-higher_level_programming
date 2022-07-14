091318cead23dec124699514fbf6166bf049343b
njorogegatua <102298471+njorogegatua@users.noreply.github.com>
2022-07-14 16:37:14 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    res = 0
-    count = 0
-    for i in sys.argv:
-        if count == 0:
-            count += 1
-            continue
-        res += int(sys.argv[count])
-        count += 1
-    print("{}".format(res))
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
